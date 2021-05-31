7d6dc55d71aebce91f1d7b75357043333fafd6c2
iKelly <irankelly850@gmail.com>
2021-05-31 13:29:25 +0200

    first

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Duhuze Hackathon</title>
+  
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+       
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+      
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+      
+        
+        
+        
+        
+        
+        <!-- Team-->
+        <section class="page-section bg-light" id="team">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">DUHUZE HACKS</h2>
+                    <h3 class="section-subheading text-muted">Kigali HighSchoolers Hack Together.</h3>
+                </div>
+                
+                <div class="row">
+                    
+                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Duhuze Hack is hackathon which is aimed to connect Kigali High school to hack for unemployment and encourage teenagers to create and learn with coding , by unlocking the power of coding by doing.Duhuze Hack is being organized by APAPER Hack club , to spread the word to all Kigali High school and leave no stone unturned.In this hackathon , APAPER Hack club wants also to encourage other high school to join Hackclub.com movement and benefit from what hack club community provides.</p>
+                    
+                    
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Clients-->
+        <div class="py-5">
+            <div class="container">
+                <div class="row align-items-center">
+                    
+                    
+                    <div class="col-md-6 col-sm-6 my-3">
+                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/codity logo yellow.png" alt="..." /></a>
+                    </div>
+                    <div class="col-md-6 col-sm-6 my-3">
+                        <a href="#!"><img class="img-fluid img-brand d-block mx-auto" src="assets/img/flag-orpheus-top.png" alt="..." /></a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
